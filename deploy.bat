3c4d7a306650fe17d85b465f1761d22cdf3a8406
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-12-28 00:14:13 +0900

    Switch to master branch before deployment

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,9 @@
 @ECHO OFF
 
+rem Switch back to the local master branch
+git checkout master
+echo Check out to the master branch on local.
+
 rem Some messages before the deployment actually starts
 echo Starting to deploy this blog website ...
 echo Proudly powered by Hexo.js at https://hexo.io/ ...
